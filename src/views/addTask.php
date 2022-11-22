88955fd95ca04604e695cd9df82e0e747f822975
Aryn Sampson <49370886+arynsampson@users.noreply.github.com>
2022-11-22 21:58:39 +0200

    add a task form

--- src/views/addTask.php
+++ src/views/addTask.php
@@ -1,5 +1,41 @@
 <?php
 
-    echo 'Page to add a task.';
-
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Task</title>
+</head>
+<body>
+    
+    <form action="addTask.php" method="POST">
+        <div>
+            <label for="username">Enter username:</label>
+            <input type="text" name="username">
+        </div>
+        <div>
+            <label for="task">Enter task:</label>
+            <input type="text" name="task">
+        </div>
+        <div>
+            <label for="due-date">Enter due date:</label>
+            <input type="text" name="due-date">
+        </div>
+        <div>
+            <label for="priority">Enter priority:</label>
+            <select name="priority" id="priority">
+                <option value=""></option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+            </select>
+        </div>
+    </form>
+
+</body>
+</html>
